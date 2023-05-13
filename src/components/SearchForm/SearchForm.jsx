f33b0eb87cd6f06266354570918e70e97c4f11e1
Danil Takiylin <danadanil900@gmail.com>
2023-05-13 14:23:07 +0200

    start project

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,38 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    serchValue: '',
+  };
+
+  handleChange = e => {
+    this.setState({ serchValue: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    if (!this.state.serchValue) {
+      window.alert('введите что то');
+      return;
+    }
+    this.props.onSubmit(this.state.serchValue);
+  };
+
   render() {
-    return <h2>SearchForm</h2>;
+    return (
+      <SearchFormStyled onSubmit={this.handleSubmit}>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+          onChange={this.handleChange}
+          value={this.state.serchValue}
+        />
+      </SearchFormStyled>
+    );
   }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,9 +4,27 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  state = {
+    serchValue: '',
+    page: 1,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { serchValue, page } = this.state;
+
+    if (prevState.serchValue !== serchValue || prevState.page !== page) {
+      ImageService.getImages(serchValue, page).then(console.log);
+    }
+  }
+
+  handleSubmit = value => {
+    this.setState({ serchValue: value });
+  };
+
   render() {
     return (
       <>
+        <SearchForm onSubmit={this.handleSubmit} />
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       </>
     );
